ab5b6ec208b4ef17d0effb976d14bb0847aeca3c
leesangyoon <pinetree408@gmail.com>
2019-12-30 11:27:06 +0900

    Update allergy problem from function based to class based

--- allergy.py
+++ allergy.py
@@ -1,65 +1,102 @@
-BEST = 0
+class Solver:
+    def __init__(self):
+        self.best = 0
 
+    def search(self, can_eat, eaters, edible, chosen):
+        if chosen >= self.best:
+            return
+        first = 0
+        while first < len(can_eat) and edible[first] > 0:
+            first = first + 1
+        if first == len(can_eat):
+            self.best = chosen
+            return
+        for i in range(len(can_eat[first])):
+            food = can_eat[first][i]
+            for j in range(len(eaters[food])):
+                edible[eaters[food][j]] = edible[eaters[food][j]] + 1
+            self.search(can_eat, eaters, edible, chosen+1)
+            for j in range(len(eaters[food])):
+                edible[eaters[food][j]] = edible[eaters[food][j]] - 1
 
-def search(can_eat, eaters, edible, chosen):
-    global BEST
-    if chosen >= BEST:
-        return
-    first = 0
-    while first < len(can_eat) and edible[first] > 0:
-        first = first + 1
-    if first == len(can_eat):
-        BEST = chosen
-        return
-    for i in range(len(can_eat[first])):
-        food = can_eat[first][i]
-        for j in range(len(eaters[food])):
-            edible[eaters[food][j]] = edible[eaters[food][j]] + 1
-        search(can_eat, eaters, edible, chosen+1)
-        for j in range(len(eaters[food])):
-            edible[eaters[food][j]] = edible[eaters[food][j]] - 1
+    def solve(self, input_case):
+        input_case_info = input_case[0].split(' ')
+        n = int(input_case_info[0])
+        m = int(input_case_info[1])
+        name_list = input_case[1].split(' ')
 
-
-def allergy(input_case):
-    global BEST
-    input_list = list(
-        map(
-            lambda x: x.strip(),
-            input_case.split('\n')
-        )
-    )
-    case_num = int(input_list[0])
-    input_list = input_list[1:]
-    case_start = 0
-    for i in range(case_num):
-        start = case_start
-        party_info = input_list[start].split(' ')
-        friend_num = int(party_info[0])
-        food_num = int(party_info[1])
-        friend_list = input_list[start+1].split(' ')
         food_info_list = []
-        for j in range(food_num):
-            food_info_list.append(input_list[start+1+j+1].split(' ')[1:])
-        can_eat = [[] for k in range(friend_num)]
-        for idx_friend, friend in enumerate(friend_list):
-            for idx_food_info, food_info in enumerate(food_info_list):
+        for i in range(m):
+            food_info_list.append(input_case[1+i+1].split(' ')[1:])
+
+        can_eat = [[] for i in range(n)]
+        for friend_idx, friend in enumerate(name_list):
+            for food_info_idx, food_info in enumerate(food_info_list):
                 if friend in food_info:
-                    can_eat[idx_friend].append(idx_food_info)
-        eaters = [[] for k in range(food_num)]
-        for idx_food_info, food_info in enumerate(food_info_list):
-            for idx_friend, friend in enumerate(friend_list):
+                    can_eat[friend_idx].append(food_info_idx)
+
+        eaters = [[] for i in range(m)]
+        for food_info_idx, food_info in enumerate(food_info_list):
+            for friend_idx, friend in enumerate(name_list):
                 if friend in food_info:
-                    eaters[idx_food_info].append(idx_friend)
-        BEST = food_num
-        edible = [0 for k in range(friend_num)]
+                    eaters[food_info_idx].append(friend_idx)
+
+        self.best = m
+        edible = [0 for i in range(n)]
         chosen = 0
-        search(can_eat, eaters, edible, chosen)
-        print BEST
-        case_start = start + 1 + food_num + 1
+        self.search(can_eat, eaters, edible, chosen)
+        return self.best
+
+
+class Tester:
+    def __parse_input_cases(self, input_cases):
+        input_case_list = list(
+            map(
+                lambda x: x.strip(),
+                input_cases.split('\n')
+            )
+        )
+        self.T = int(input_case_list[0])
+        input_case_list = input_case_list[1:]
+        cursor = 0
+        parsed_input_case_list = []
+        for i in range(self.T):
+            input_info = input_case_list[cursor].split(' ')
+            m = int(input_info[1])
+            parsed_input_case = input_case_list[cursor:cursor+1+m+1]
+            parsed_input_case_list.append(parsed_input_case)
+            cursor = cursor+1+m+1
+        return parsed_input_case_list
+
+    def __parse_output_cases(self, output_cases):
+        output_case_list = list(
+            map(
+                lambda x: int(x.strip()),
+                output_cases.split('\n')
+            )
+        )
+        return output_case_list[1:]
+
+    def set_input_cases(self, input_cases):
+        self.input_case_list = self.__parse_input_cases(input_cases)
+
+    def set_output_cases(self, output_cases):
+        self.output_case_list = self.__parse_output_cases(output_cases)
+
+    def test_solver(self, solver):
+        print("Answer\tResult\tIsCorrect")
+        for t in range(self.T):
+            result = solver.solve(self.input_case_list[t])
+            answer = self.output_case_list[t]
+            print(str(answer)+"\t"+str(result)+"\t"+str(answer == result))
 
 
 if __name__ == '__main__':
-    input_case = \
+    '''
+    problem link: https://algospot.com/judge/problem/read/ALLERGY
+    '''
+
+    input_cases = \
         '''2
         4 6
         cl bom dara minzy
@@ -78,4 +115,16 @@ if __name__ == '__main__':
         5 b c f h i
         4 b e g j
         5 b c g h i'''
-    allergy(input_case)
+
+    output_cases = \
+        '''2
+        2
+        3'''
+
+    tester = Tester()
+    solver = Solver()
+
+    tester.set_input_cases(input_cases)
+    tester.set_output_cases(output_cases)
+
+    tester.test_solver(solver)
